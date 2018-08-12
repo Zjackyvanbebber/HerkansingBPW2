8d55e1ffb4ff2c987318d5c6eaccc3eab828eb23
Unknown <zjackyphotography@live.nl>
2018-08-12 22:24:18 +0200

    [+] Part 3

---
+++ Assets/DestroySelf.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+    private GameObject self;
+
+
+    private void Start()
+    {
+        self = gameObject;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Destroy(self);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(self);
+        }
+    }
+}
